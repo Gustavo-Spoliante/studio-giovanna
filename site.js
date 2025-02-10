52c1e822b27d7c44760de44a7d8fc45da92bf5e3
Gustavo-Spoliante <166967941+Gustavo-Spoliante@users.noreply.github.com>
2025-02-10 19:52:29 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,13 @@
+
+
+const principalEl = document.querySelector('.principal')
+const contatos1 = document.querySelector('.contatos')
+
+function contatosEL () {
+ 
+    principalEl.style.display = "none"
+    
+}
+
+
+
